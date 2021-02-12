7638739c3674d9e80c03c84137cc9700979fad3c
Christopher Kim <chris.kim1990@gmail.com>
2021-02-12 20:36:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   lowest_number = nil
   name_hash.each do |index|
     index = 0
-    if lowest number == nil || name_hash[index] < lowest_number
+    if lowest_number == nil || name_hash[index] < lowest_number
       lowest_number = name_hash[index]
     end
     lowest_number

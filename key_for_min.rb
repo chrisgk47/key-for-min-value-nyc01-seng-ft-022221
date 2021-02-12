354db31ee10a93e0923250cb5224e8cdc1114158
Christopher Kim <chris.kim1990@gmail.com>
2021-02-12 20:00:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   new_hash = {}
   name_hash.each do |index|
     if name_hash[index] < name_hash[index + 1]
-      new_hash << name_hash[index][0]
+      new_hash << name_hash[index][0].to_i
     end
     new_hash
   end

5b05172db9df8edab6f9355ca770343eea930e18
Christopher Kim <chris.kim1990@gmail.com>
2021-02-12 21:41:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.each do |title, age|
-    if name_hash[title][age] < 500
-      puts name_hash[title]
+  name_hash.each do |k, v|
+    if name_hash[k][v] < 500
+      puts name_hash[k]
     end
   end
-  name_hash[title]
+  name_hash
 end
